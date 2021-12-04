dba3162307a313c1e2a6de2a769ba0a01dc28d47
Sayantan Sarkar <sayskar500@Sayskar500.localdomain>
2021-12-04 15:35:23 +0530

    added fetch functions for orion and mirror protocol

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import { Headers } from "node-fetch";
+
 const { MessageEmbed, WebhookClient } = require('discord.js');
 const config = require('./config.json');
 const ftch = require('node-fetch');
@@ -5,28 +7,38 @@ const ftch = require('node-fetch');
 const webhookClient = new WebhookClient({ id: config.id, token: config.token });
 
 var map = new Map();
-
+const embed = new MessageEmbed().setTitle('NEW STAGE !').setColor('#0099ff');
 
 /* Function to check for new Anchor Airdrop */
 
 const fetchAnchorAirdrop = async (airdropUrl: any, protocolName: any) => {
-  const embed = new MessageEmbed().setTitle('NEW STAGE !').setColor('#0099ff');
   return ftch(airdropUrl)
     .then((res: { json: () => any; }) => res.json())
     .then((ans: { [x: string]: { stage: any; }; }) => {
 
+      var newStage = [];
+
       let mx = -1;
       for (var w in ans) {
         const currentStage = ans[w].stage;
+        newStage.push(currentStage);
         mx = Math.max(mx, currentStage);
       }
 
       if (map.has(protocolName)) {
         const prevStage = map.get(protocolName);
+        let totalAmount = 0;
+
         if (mx > prevStage) {
           map.delete(protocolName);
           map.set(protocolName, mx);
-          const embed1 = new MessageEmbed().setTitle('New Stage has appeared for anchor protocol:').setColor('#0099ff');
+
+          for (var z in newStage) {
+            const currentAmount = newStage[z].amount;
+            if (currentAmount > prevStage) totalAmount += currentAmount;
+          }
+
+          const embed1 = new MessageEmbed().setTitle(`New Stage/Stages have appeared for anchor protocol providing a total amount of ${totalAmount}`).setColor('#0099ff');
           webhookClient.send({
             //content: `New Stage has appeared for anchor protocol: ${ res } `,
             username: 'Stader - Bot',
@@ -41,49 +53,135 @@ const fetchAnchorAirdrop = async (airdropUrl: any, protocolName: any) => {
 
 
 
+
+/* Function to check for new Orion Airdrop */
+
+const fetchOrionAirdrop = async (airdropUrl: any, protocolName: any) => {
+  return ftch(airdropUrl)
+    .then((res: { json: () => any; }) => res.json())
+    .then((ans: { claim: any; }) => {
+      const oriondrops = ans.claim;
+      const mx = oriondrops.stage;
+      const totalAmount = oriondrops.amount;
+
+      if (map.has(protocolName)) {
+        const prevStage = map.get(protocolName);
+        if (mx > prevStage) {
+          map.delete(protocolName);
+          map.set(protocolName, mx);
+          const embed1 = new MessageEmbed().setTitle(`New Stage has appeared for anchor protocol providing a total amount of ${totalAmount}`).setColor('#0099ff');
+          webhookClient.send({
+            //content: `New Stage has appeared for anchor protocol: ${ res } `,
+            username: 'Stader - Bot',
+            avatarURL: 'https://imgur.com/4Zi1nQd',
+            embeds: [embed, embed1],
+          });
+        }
+      }
+      else map.set(protocolName, mx);
+    });
+}
+
+
+
+
 /* Function to check for new Mirror Airdrop */
 
-// const fetchMirrorAirdrop = async (fig, protocol_name) => {
-//   return ftch("https://graph.mirror.finance/graphql", {
-//     method: "POST",
-//     headers: { "Content-Type": "application/json" },
-//     body: JSON.stringify({
-//       query: `
-//           airdrop($address: String!, $network: String = \"TERRA\") {\n    airdrop(address: $address, network: $network)\n}
-//           "{\n    \"address\": \"terra1pdpkhjf3lzshe9kv884l6hchdxrulcxy4x2676\",\n    \"network\": \"TERRA\"\n}"
-//           `,
-//     }),
-//   })
-//     .then((body) => {
-//       console.log(body); // result
-//     });
-// }
+const fetchMirrorAirdrop = async (airdropUrl: any, protocolName: any) => {
+  var myHeader = new Headers();
+  myHeader.append("Content-Type", "application/json");
+
+  var graphql = JSON.stringify({
+    query: "query airdrop(\r\n    $address: String!, \r\n    $network: String!){\r\n        airdrop(\r\n            address: $address, \r\n            network: $network)\r\n        },",
+    variables: { "address": "terra1pdpkhjf3lzshe9kv884l6hchdxrulcxy4x2676", "network": "TERRA" }
+  })
+
+  var requestOptions = {
+    method: 'POST',
+    headers: myHeader,
+    body: graphql,
+    redirect: 'follow'
+  };
+
+  ftch(airdropUrl, requestOptions)
+    .then((response: { json: () => any; }) => response.json())
+    .then((res: { data: { airdrop: any; }; }) => {
+      const mirrorAirdrops = res.data.airdrop;
+      const bbc = mirrorAirdrops.length;
+      //console.log(bbc);
+      if (bbc > 0) {
+        var newStage = [];
+
+        let mx = -1;
+
+        for (var w in mirrorAirdrops) {
+          const currentStage = mirrorAirdrops[w].stage;
+          newStage.push(currentStage);
+          mx = Math.max(mx, currentStage);
+        }
+
+        if (map.has(protocolName)) {
+          const prevStage = map.get(protocolName);
+          let totalAmount = 0;
+
+          if (mx > prevStage) {
+            map.delete(protocolName);
+            map.set(protocolName, mx);
+
+            for (var z in newStage) {
+              const currentAmount = newStage[z].amount;
+              if (currentAmount > prevStage) totalAmount += currentAmount;
+            }
+
+            const embed1 = new MessageEmbed().setTitle(`New Stage has appeared for pylon protocol providing a total amount of ${totalAmount}`).setColor('#0099ff');
+            webhookClient.send({
+              //content: `New Stage has appeared for pylon protocol: ${res}`,
+              username: 'Stader-Bot',
+              avatarURL: 'https://imgur.com/4Zi1nQd',
+              embeds: [embed, embed1],
+            });
+          }
+        }
+        else map.set(protocolName, mx);
+      }
+    });
+}
+
+
 
 
 
 /* Function to check for new Pylon Airdrop */
 
 const fetchPylonAirdrop = async (airdropUrl: any, protocolName: any) => {
-  const embed = new MessageEmbed().setTitle('NEW STAGE !').setColor('#0099ff');
+
   return ftch(airdropUrl)
     .then((res: { json: () => any; }) => res.json())
     .then((ans: { claimableAirdrops: any; }) => {
 
       const protocolUrl = ans.claimableAirdrops;
+      var newStage = [];
       let mx = -1;
 
       for (var w in protocolUrl) {
         const currentStage = protocolUrl[w].stage;
+        newStage.push(currentStage);
         mx = Math.max(mx, currentStage);
       }
 
       if (map.has(protocolName)) {
         const prevStage = map.get(protocolName);
+        let totalAmount = 0;
+
+        for (var z in newStage) {
+          const currentAmount = newStage[z].amount;
+          if (currentAmount > prevStage) totalAmount += currentAmount;
+        }
 
         if (mx > prevStage) {
           map.delete(protocolName);
           map.set(protocolName, mx);
-          const embed1 = new MessageEmbed().setTitle('New Stage has appeared for pylon protocol').setColor('#0099ff');
+          const embed1 = new MessageEmbed().setTitle(`New Stage has appeared for pylon protocol providing a total amount of ${totalAmount}`).setColor('#0099ff');
           webhookClient.send({
             //content: `New Stage has appeared for pylon protocol: ${res}`,
             username: 'Stader-Bot',
@@ -98,28 +196,40 @@ const fetchPylonAirdrop = async (airdropUrl: any, protocolName: any) => {
 
 
 
+
+
 /* Function to check for new Valkyrie Airdrop */
 
 const fetchValkyrieAirdrop = async (airdropUrl: any, protocolName: any) => {
-  const embed = new MessageEmbed().setTitle('NEW STAGE !').setColor('#0099ff');
+
   return ftch(airdropUrl)
     .then((res: { json: () => any; }) => res.json())
     .then((ans: { data: { items: any; }; }) => {
 
       const protocolUrl = ans.data.items;
+
+      var newStage = [];
       let mx = -1;
 
       for (var w in protocolUrl) {
         const currentStage = protocolUrl[w].stage;
+        newStage.push(currentStage);
         mx = Math.max(mx, currentStage);
       }
 
       if (map.has(protocolName)) {
         const prevStage = map.get(protocolName);
+        let totalAmount = 0;
+
+        for (var z in newStage) {
+          const currentAmount = newStage[z].amount;
+          if (currentAmount > prevStage) totalAmount += currentAmount;
+        }
+
         if (mx > prevStage) {
           map.delete(protocolName);
           map.set(protocolName, mx);
-          const embed1 = new MessageEmbed().setTitle('New Stage has appeared for valkyrie protocol').setColor('#0099ff');
+          const embed1 = new MessageEmbed().setTitle(`New Stage has appeared for valkyrie protocol providing a total amount of ${totalAmount}`).setColor('#0099ff');
           webhookClient.send({
             //content: `New Stage has appeared for valkyrie protocol: ${ res } `,
             username: 'Stader-Bot',
@@ -134,6 +244,7 @@ const fetchValkyrieAirdrop = async (airdropUrl: any, protocolName: any) => {
 
 
 
+
 function stage_new() {
   return ftch("https://www.postman.com/collections/cc2042cc71d5ce36ef30")
     .then((res: { json: () => any; }) => res.json())
@@ -141,12 +252,12 @@ function stage_new() {
       const postmanUrl = text.item;
 
       for (var x in postmanUrl) {
-        console.log(postmanUrl[x].name);
+        //console.log(postmanUrl[x].name);
 
         if (postmanUrl[x].name === "Mirror Airdrops") {
           const airdropUrl = postmanUrl[x].request.url.raw;
           const protocolName = postmanUrl[x].name;
-          //await fetchMirrorAirdrop(fig, protocol_name);
+          await fetchMirrorAirdrop(airdropUrl, protocolName);
         }
 
         if (postmanUrl[x].name === "Anchor Airdrops") {
@@ -168,6 +279,13 @@ function stage_new() {
           const protocolName = postmanUrl[x].name;
           await fetchValkyrieAirdrop(airdropUrl, protocolName);
         }
+
+        if (postmanUrl[x].name === "Orion Airdrops Proofs") {
+          const airdropUrl = postmanUrl[x].request.url.raw;
+          const protocolName = postmanUrl[x].name;
+          await fetchOrionAirdrop(airdropUrl, protocolName);
+        }
+
       }
     });
 }
